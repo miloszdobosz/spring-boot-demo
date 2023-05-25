ab7970e8c62f2992a8bb0613c2e7f01de4917d48
mi <milosz.dobosz@emcps.eu>
2023-05-25 12:32:30 +0200

    Formatted

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -9,9 +9,4 @@ public class SpringBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
-
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.springbootdemo")).build();
-//    }
 }
--- src/main/java/com/example/springbootdemo/messages/Item.java
+++ src/main/java/com/example/springbootdemo/messages/Item.java
@@ -1,3 +1,4 @@
 package com.example.springbootdemo.messages;
 
-public record Item(int category_id, int item_id, String name) { }
+public record Item(int category_id, int item_id, String name) {
+}
