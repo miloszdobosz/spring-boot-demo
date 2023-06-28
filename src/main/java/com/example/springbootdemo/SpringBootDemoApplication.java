a9102a3e71348718b14a504e83ff70b92ce823ef
mi <milosz.dobosz@emcps.eu>
2023-06-28 11:35:24 +0200

    New API UI works at /api.html

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -1,9 +1,18 @@
 package com.example.springbootdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Demo REST API",
+                version = "1.0",
+                description = "This is the best API in existence."
+        )
+)
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
