e165300d0e242afc4a6e651e9d5bd5ad404441e4
mi <milosz.dobosz@emcps.eu>
2023-07-04 14:42:47 +0200

    Redirection

--- src/main/java/com/example/springbootdemo/security/ExceptionHandlerFilter.java
+++ src/main/java/com/example/springbootdemo/security/ExceptionHandlerFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,15 +13,22 @@ import java.io.IOException;
 
 @Component
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
-
     @Override
     public void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
         } catch (PreAuthenticatedCredentialsNotFoundException e) {
-            response.setStatus(HttpStatus.BAD_REQUEST.value());
-            response.getWriter().write(e.getMessage());
+            // No field "Authorization" in request header
+
+            // Solution 1: Tomcat error page
 //            response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+
+            // Solution 2: Plain string error page
+//            response.setStatus(HttpStatus.BAD_REQUEST.value());
+//            response.getWriter().write(e.getMessage());
+
+            // Solution 3: Redirection
+            response.sendRedirect("/api.html");
         }
     }
 }
